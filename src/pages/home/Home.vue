8387c077c7ad92a5f91e07c874bbd792b2c1e3ee
e <aowoiiii@vip.qq.com>
2019-02-23 23:53:18 +0800

    Revert "swiper"
    This reverts commit d5368f82c5c973997e4f946ccce1ba6f28cce451.

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div>
-    <home-header></home-header>
-    <home-swiper></home-swiper>
+    <home-header>
+
+    </home-header>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
-import HomeSwiper from './components/Swiper'
 export default {
   name: 'Home',
-  components: {
-    HomeHeader,
-    HomeSwiper}
+  components: {HomeHeader}
 }
 </script>
 
